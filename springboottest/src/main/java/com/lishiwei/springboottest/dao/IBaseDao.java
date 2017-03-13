7d78bb00cc1c6de45eda43db2489cf2c88912cfd
lishiwei <lishiwei@oneapm.com>
2017-03-13 20:34:38 +0800

    add deploy

--- springboottest/src/main/java/com/lishiwei/springboottest/dao/IBaseDao.java
+++
@@ -1,8 +0,0 @@
-package com.lishiwei.springboottest.dao;
-
-/**
- * Created by lishiwei on 17-3-10.
- */
-public interface IBaseDao<T> {
-    int query(T t);
-}
--- springtest/src/main/java/com/lishiwei/springtest/controller/LoginController.java
+++ springtest/src/main/java/com/lishiwei/springtest/controller/LoginController.java
@@ -1,7 +1,20 @@
 package com.lishiwei.springtest.controller;
 
+import com.lishiwei.springtest.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by lishiwei on 17-3-13.
  */
+@Controller
 public class LoginController {
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/login.do")
+    public String loginPage() {
+        return "login";
+    }
 }
